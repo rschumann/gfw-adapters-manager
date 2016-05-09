eda7e2ca0d050ec861d4eae305750ad38fb29b84
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-05-09 11:47:02 +0200

    Set redis for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,7 @@
-host   = ENV.fetch('REDIS_HOST')     { 'localhost' }
-port   = ENV.fetch('REDIS_PORT')     { 6379 }
-$redis = Redis.new(host: host, port: port)
+if ENV["REDISCLOUD_URL"]
+    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  host   = ENV.fetch('REDIS_HOST')     { 'localhost' }
+  port   = ENV.fetch('REDIS_PORT')     { 6379 }
+  $redis = Redis.new(host: host, port: port)
+end
