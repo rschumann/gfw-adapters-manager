9393d5e9c2b33eab9718ddec41efc9e32671caa1
Sebastian Schkudlara <sebastian.schkudlara@gmail.com>
2016-05-13 13:30:49 +0200

    Fixes for mailer config

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 if ENV["REDISCLOUD_URL"].present?
   $redis = Redis.new(url: ENV["REDISCLOUD_URL"])
 else
-  host   = ENV.fetch('REDIS_HOST')     { 'localhost' }
-  port   = ENV.fetch('REDIS_PORT')     { 6379 }
+  host   = ENV.fetch('REDIS_HOST') { 'localhost' }
+  port   = ENV.fetch('REDIS_PORT') { 6379 }
   $redis = Redis.new(host: host, port: port)
 end
